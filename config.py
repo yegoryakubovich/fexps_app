4159781e5a44bea9f4e9e3901fa830cdbdff0ead
KEKACIK <arthur_0p@mail.ru>
2024-03-06 14:01:57 +0300

    Add datetime format to config

--- config.py
+++ config.py
@@ -28,6 +28,7 @@ class Settings(BaseSettings):
     url: str  # = 'http://api.test.mybody.one'
     url_telegram: str = 'https://t.me/fexps_manager'
     privacy_policy_article_id: int = 1
+    datetime_format: str = '%d-%m-%y %H:%M'
 
     model_config = SettingsConfigDict(env_file='.env')
 
