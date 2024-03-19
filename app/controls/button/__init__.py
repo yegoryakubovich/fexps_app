541e8ffa85624322e328579d079aaffb17d75c0c
KEKACIK <arthur_0p@mail.ru>
2024-03-19 17:32:23 +0300

    minor fix

--- app/controls/button/__init__.py
+++ app/controls/button/__init__.py
@@ -15,8 +15,9 @@
 #
 
 
+from .standart import StandardButton
+from .chip import Chip
 from .filled import FilledButton
 from .list_item import ListItemButton
-from .chip import Chip
 from .scopes import ActionItem, ActionCard, Action
 from .switch import StitchButton
