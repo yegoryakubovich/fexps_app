e5d92b25e70ccd6acc3ec822f7bbf911483b2531
KEKACIK <arthur_0p@mail.ru>
2024-03-19 18:15:09 +0300

    Fix buttons wallet select and new create, edit_name

---
+++ app/views/client/balance/wallets_create.py
@@ -0,0 +1,63 @@
+#
+# (c) 2024, Yegor Yakubovich, yegoryakubovich.com, personal@yegoryakybovich.com
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+from flet_core import Column, Container, padding, colors, Row
+
+from app.controls.button import StandardButton
+from app.controls.input import TextField
+from app.controls.layout import ClientBaseView
+
+
+class WalletCreateView(ClientBaseView):
+    route = '/client/wallets/create'
+    tf_name: TextField
+
+    async def build(self):
+        self.tf_name = TextField(
+            label=await self.client.session.gtv(key='wallet_name'),
+            value='Default',
+        )
+        controls = [
+            Container(
+                content=Column(
+                    controls=[
+                        self.tf_name,
+                        Row(
+                            controls=[
+                                StandardButton(
+                                    text=await self.client.session.gtv(key='create'),
+                                    on_click=self.create,
+                                    expand=True,
+                                    bgcolor=colors.SECONDARY,
+                                ),
+                            ],
+                        ),
+                    ],
+                ),
+                padding=padding.only(bottom=15),
+            )
+        ]
+        self.controls = await self.get_controls(
+            title=await self.client.session.gtv(key='wallet_create'),
+            main_section_controls=controls,
+        )
+
+    async def go_back(self, _):
+        await self.client.change_view(go_back=True, delete_current=True, with_restart=True)
+
+    async def create(self, _):
+        await self.client.session.api.client.wallets.create(name=self.tf_name.value)
+        await self.client.change_view(go_back=True, delete_current=True, with_restart=True)
--- app/views/client/balance/wallets_select.py
+++ app/views/client/balance/wallets_select.py
@@ -13,19 +13,23 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+
+
 from functools import partial
 
-from flet_core import Column, Container, padding, colors, Row, MainAxisAlignment, alignment
+from flet_core import Column, Container, padding, colors, Row, MainAxisAlignment
 
 from app.controls.button import StandardButton
 from app.controls.information import Text
 from app.controls.layout import ClientBaseView
 from app.utils import Fonts
+from app.views.client.balance import WalletCreateView
+from app.views.client.balance.wallets_name_edit import WalletNameEditView
 from config import settings
 
 
 class WalletSelectView(ClientBaseView):
-    route = '/client/wallet/select'
+    route = '/client/wallets/select'
     wallets: list
     wallets_column: Column
     selected_wallet_id: int
@@ -56,7 +60,7 @@ class WalletSelectView(ClientBaseView):
                     alignment=MainAxisAlignment.SPACE_BETWEEN,
                 ),
                 bgcolor=bgcolor,
-                vertical=100,
+                vertical=24,
                 on_click=partial(self.select_wallet, wallet.id),
                 expand=True,
             )]))
@@ -66,20 +70,19 @@ class WalletSelectView(ClientBaseView):
         # self.client.session.account
         await self.set_type(loading=True)
         self.wallets = await self.client.session.api.client.wallets.get_list()
-        await self.set_type(loading=False)
         self.selected_wallet_id = self.client.session.current_wallet.id
         self.wallets_column = Column(controls=await self.get_wallet_list())
-
+        await self.set_type(loading=False)
         controls = [
-            self.wallets_column,
             Container(
                 content=Column(
                     controls=[
+                        self.wallets_column,
                         Row(
                             controls=[
                                 StandardButton(
                                     text=await self.client.session.gtv(key='edit_name'),
-                                    on_click=None,
+                                    on_click=self.edit_name,
                                     expand=True,
                                     bgcolor=colors.GREY_400,
                                 ),
@@ -96,7 +99,7 @@ class WalletSelectView(ClientBaseView):
                             controls=[
                                 StandardButton(
                                     text=await self.client.session.gtv(key='create_new_wallet'),
-                                    on_click=None,
+                                    on_click=self.create,
                                     expand=True,
                                     bgcolor=colors.GREY_400,
                                 ),
@@ -105,7 +108,7 @@ class WalletSelectView(ClientBaseView):
                         Row(
                             controls=[
                                 StandardButton(
-                                    text=await self.client.session.gtv(key='Select'),
+                                    text=await self.client.session.gtv(key='select'),
                                     on_click=self.switch_wallet,
                                     expand=True,
                                 ),
@@ -114,14 +117,10 @@ class WalletSelectView(ClientBaseView):
                     ],
                 ),
                 padding=padding.only(bottom=15),
-                alignment=alignment.bottom_center,
-                expand=True,
-
             )
         ]
         self.controls = await self.get_controls(
             title=await self.client.session.gtv(key='wallets'),
-            with_expand=True,
             main_section_controls=controls,
         )
 
@@ -139,3 +138,9 @@ class WalletSelectView(ClientBaseView):
                 self.client.session.current_wallet = wallet
                 await self.client.session.set_cs(key='current_wallet', value=self.client.session.current_wallet)
         await self.client.change_view(go_back=True, delete_current=True, with_restart=True)
+
+    async def create(self, _):
+        await self.client.change_view(view=WalletCreateView())
+
+    async def edit_name(self, _):
+        await self.client.change_view(view=WalletNameEditView(wallet_id=self.selected_wallet_id))
